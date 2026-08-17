4a6cf42fa16ed05370e70c34e0be00d084738591
Tit Lymeng <titlymeng720@gmail.com>
2026-08-17 15:36:24 +0700

    frontend/todolist

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,10 +2,9 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true, 
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
-    withCredentials: true,
   },
 });
 
